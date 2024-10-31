3301580dca97538670a8b123567e5c7ab4d0f786
carlitoselmago <ccarbonell@gmail.com>
2024-10-31 01:11:16 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
-$( document ).ready(function() {
-   
+$(document).ready(function() {
+    $(".openform").click(function(event) {
+        event.preventDefault(); // Evita el comportamiento por defecto del enlace
+        $(".form").css("display","flex"); // Alterna la visibilidad del formulario
+    });
 });
